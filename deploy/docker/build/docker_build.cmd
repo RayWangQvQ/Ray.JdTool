0ac2ed6d41c36dbbfb7554b3125e017ebd34501f
Ray <2271272653@qq.com>
2021-09-06 01:39:26 +0800

    Update

---
+++ deploy/docker/build/docker_build.cmd
@@ -0,0 +1,5 @@
+@echo off
+cd /d ../../..
+docker build -f src/Ray.JdTool.HttpApi.Host/Dockerfile -t zai7lou/jd_tool_api .
+docker build -f src/Ray.JdTool.Blazor/Dockerfile -t zai7lou/jd_tool_web .
+pause
