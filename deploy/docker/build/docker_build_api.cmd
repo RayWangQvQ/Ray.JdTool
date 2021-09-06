6da6822c1d676b101ee2d62af8e6deec46eab1b4
Ray <2271272653@qq.com>
2021-09-06 21:31:18 +0800

    完善Blazor的配置

--- deploy/docker/build/docker_build_api.cmd
+++ deploy/docker/build/docker_build_api.cmd
@@ -1,5 +1,4 @@
 @echo off
 cd /d ../../..
 docker build -f src/Ray.JdTool.HttpApi.Host/Dockerfile -t zai7lou/jd_tool_api .
-docker build -f src/Ray.JdTool.Blazor/Dockerfile -t zai7lou/jd_tool_web .
 pause
