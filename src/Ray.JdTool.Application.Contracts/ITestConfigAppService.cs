4084f59d22b67c79d55d61e032e34030f6a0d862
RayWang <2271272653@qq.com>
2021-09-06 18:47:13 +0800

    Update

---
+++ src/Ray.JdTool.Application.Contracts/ITestConfigAppService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Application.Services;
+
+namespace Ray.JdTool
+{
+    public interface ITestConfigAppService : IApplicationService, IRemoteService
+    {
+        string Test(string configKey);
+    }
+}
---
+++ src/Ray.JdTool.Application/TestConfigAppService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Http.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace Ray.JdTool
+{
+    public class TestConfigAppService : JdToolAppService, ITestConfigAppService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TestConfigAppService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Test(string configKey)
+        {
+            return _configuration[configKey];
+        }
+    }
+}
